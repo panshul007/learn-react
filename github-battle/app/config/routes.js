a36de78bfa78ad885fe533ce8346c5e9f5f249be
Panshul Gupta <panshul007@gmail.com>
2016-05-08 00:25:39 +0200

    added prompt container to routes with route props.

--- github-battle/app/config/routes.js
+++ github-battle/app/config/routes.js
@@ -6,12 +6,15 @@ var IndexRoute = ReactRouter.IndexRoute;
 var hashHistory = ReactRouter.hashHistory;
 var Main = require('../components/Main');
 var Home = require('../components/Home');
+var PromptContainer = require('../containers/PromptContainer')
 
 var routes = (
   <Router history ={hashHistory}>
     <Route path='/'  component={Main}>
       <Route path='/home' component={Home} />
       <IndexRoute component={Home} /> // when none of the routes match, this route will be loaded as default.
+      <Route path='playerOne' header='Player One' component={PromptContainer} />
+      <Route path='playerTwo/:playerOne' header='Player Two' component={PromptContainer} />
     </Route>
   </Router>
 );
