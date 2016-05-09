1bff1aeaa0d24adc8652c355fc701ac76dc4ac2a
Panshul Gupta <panshul.gupta@commercetools.de>
2016-05-09 17:59:29 +0200

    render results page, refactor common code into component.

--- github-battle/app/components/Prompt.js
+++ github-battle/app/components/Prompt.js
@@ -1,11 +1,11 @@
 var React = require('react');
-var transparentBg = require('../styles').transparentBg;
 var PropTypes = React.PropTypes;
+var MainContainer = require('./MainContainer');
 
 // functional stateless components
 function Prompt (props) {
   return (
-    <div className = "jumbotron col-sm-6 col-sm-offset3 text-center" style={transparentBg}>
+    <MainContainer>
       <h1>{props.header}</h1>
       <div className="col-sm-12">
         <form onSubmit={props.onSubmitUser}>
@@ -26,7 +26,7 @@ function Prompt (props) {
           </div>
         </form>
       </div>
-    </div>
+    </MainContainer>
   )
 }
 
