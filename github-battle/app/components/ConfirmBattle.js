d3dd8007b2e41c208dddb3e31780329c7bf968a9
Panshul Gupta <panshul.gupta@commercetools.de>
2016-05-09 19:13:46 +0200

    adding css and transitional animations.

--- github-battle/app/components/ConfirmBattle.js
+++ github-battle/app/components/ConfirmBattle.js
@@ -10,7 +10,7 @@ var Loading = require('./Loading');
 function ConfirmBattle (props) {
   return (
     props.isLoading === true
-      ? <p> <Loading speed={800} text={'Waiting'}/> </p>
+      ? <Loading speed={800} text={'Waiting'}/>
       : <div className="jumbotron col-sm-12 text-center" style={styles.transparentBg}>
         <h1>Confirm Players</h1>
         <div className='col-sm-8 col-sm-offset-2'>
